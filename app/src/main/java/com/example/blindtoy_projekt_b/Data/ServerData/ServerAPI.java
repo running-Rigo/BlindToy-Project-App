e6d9d83f29ed9ef7b80dce39b2a71a18b1672024
running-Rigo <114597348+running-Rigo@users.noreply.github.com>
2023-05-02 13:46:34 +0200

    2.4.23

--- app/src/main/java/com/example/blindtoy_projekt_b/Data/ServerData/ServerAPI.java
+++ app/src/main/java/com/example/blindtoy_projekt_b/Data/ServerData/ServerAPI.java
@@ -6,31 +6,32 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ServerAPI {
     @FormUrlEncoded
-    @POST("checkUserLogin.php")
+    @POST("T5gV5gUmMA5sPh54scf.php") //checkUserLogin.php
     Call <ServerDbUser> checkUserLogin( //Method checkUserLogin checks if the username & password are within the database;
                                         @Field("email") String email,
                                         @Field("password") String password);
 
     @FormUrlEncoded
-    @POST("checkUserLogin.php")
+    @POST("T5gV5gUmMA5sPh54scf.php")
     Call <ServerDbUser> checkUserToken(
             @Field("token") String token,
             @Field("user_id") String userId
     );
 
     @FormUrlEncoded
-    @POST("registerNewUser.php")
+    @POST("6y2VV3dKMgZi6p7To6a.php")  //registerNewUser.php
     Call<String>  registerNewUser( //Posts new user credentials to the external database
                                         @Field("name") String username,
                                         @Field("email") String email,
                                         @Field("password") String password);
 
     @FormUrlEncoded
-    @POST("savePet.php")
+    @POST("St3t4t65ry7oky84vSR.php") //savePet.php
     Call<String>  saveNewPet(
             @Field("name") String name,
             @Field("species") String species,
@@ -40,7 +41,7 @@ public interface ServerAPI {
     );
 
     @FormUrlEncoded
-    @POST("deletePet.php")
+    @POST("Dz2vYU342pL88xVgw4M.php") //deletePet.php
     Call<String> deleteOnePet(
             @Field("pet_id") String petId,
             @Field("token") String token,
@@ -48,7 +49,7 @@ public interface ServerAPI {
     );
 
     @FormUrlEncoded
-    @POST("changePet.php")
+    @POST("Ua2DyFuv7vt63wZ54Z4.php") //changePet.php
     Call<String> saveNewSounds(
             @Field("pet_id") String petId,
             @Field("sounds") String sounds,
@@ -56,3 +57,7 @@ public interface ServerAPI {
             @Field("user_id") String userId
     );
     }
+
+
+    // authentification.php -> Ua2DyFuv7vt63wZ54Z3.php; db_settings_blinddog -> 9e7tz9666cEzTY5Fuay.php; db_script.php -> 7LKDo6vB8qVEF2y4yb2
+
--- app/src/main/java/com/example/blindtoy_projekt_b/Views/Login/RegistrationFragment.java
+++ app/src/main/java/com/example/blindtoy_projekt_b/Views/Login/RegistrationFragment.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.text.method.HideReturnsTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
